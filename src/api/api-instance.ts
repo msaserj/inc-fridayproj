cc83fdf9d2687e4ea725fa6f384bfc15a347a25f
msaserj <serg.ks@gmail.com>
2022-08-26 20:52:01 +0300

    fix change name

--- src/api/api-instance.ts
+++ src/api/api-instance.ts
@@ -4,11 +4,11 @@ import axios from "axios";
 export const instanceLocal = axios.create({
     // baseURL: process.env.REACT_APP_BACK_URL || 'http://localhost:7542/2.0/',
     // для запросов на смену пароля менять урл на https://neko-back.herokuapp.com/2.0/
-    baseURL: 'http://localhost:7542/2.0/',
+    baseURL: 'https://neko-back.herokuapp.com/2.0/',
     withCredentials: true,
 })
 
 export const instanceExternal = axios.create({
-    baseURL: 'http://localhost:7542/2.0/',
+    baseURL: 'https://neko-back.herokuapp.com/2.0/',
     withCredentials: true,
 })
