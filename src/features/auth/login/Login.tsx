fa9bafcfad254e5c1eb0084b2c3450e9ed9a82f1
msaserj <serg.ks@gmail.com>
2022-09-01 21:43:20 +0300

    Refactoring Login

--- src/features/auth/login/Login.tsx
+++ src/features/auth/login/Login.tsx
@@ -3,25 +3,21 @@ import {PATH} from "../../../common/constants/Path";
 import {useAppSelector} from "../../../common/hooks/hooks";
 import {LoginForm} from "./LoginForm";
 import css from "./Login.module.css";
-import IT_Incubator from '../../../assets/img/Group 753.svg'
 
 
 export const Login = () => {
 	const user_ID = useAppSelector(state => state.auth.user._id);
 	if (user_ID) {
-		return <Navigate to={PATH.PROFILE}/>
+		return <Navigate to={PATH.PACK_LIST}/>
 	}
-
 	return (
-		<div className={css.smallContainer}>
-			 <img src={IT_Incubator} alt="IT_Incubator"/>
-			<h2>Sign In</h2>
-			<LoginForm/>
-
+		<div className={css.mainBlock}>
+			 {/*<img src={IT_Incubator} alt="IT_Incubator"/>*/}
+			<div className={css.loginBlock}>
+				<h2>Sign In</h2>
+				<LoginForm/>
+			</div>
 		</div>
-
 	)
-
-
 }
 
--- src/features/auth/login/LoginForm.tsx
+++ src/features/auth/login/LoginForm.tsx
@@ -1,15 +1,14 @@
-import React, {useState} from 'react';
+import React from 'react';
 import SuperButton from "../../../common/components/c2-Button/SuperButton";
 import {PATH} from "../../../common/constants/Path";
 import {useAppDispatch} from "../../../common/hooks/hooks";
 import {NavLink} from "react-router-dom";
 import s from './Login.module.css';
 import {useFormik} from "formik";
-import {FormControl, IconButton, InputAdornment, InputLabel, OutlinedInput} from "@mui/material";
-import {Visibility, VisibilityOff} from "@mui/icons-material";
 import SuperInputText from "../../../common/components/c1-InputText/SuperInputText";
 import SuperCheckbox from "../../../common/components/c3-Checkbox/SuperCheckbox";
 import {loginThunkTC} from "../auth-reducer";
+import SuperPasswordInput from "../../../common/components/c1-InputText/PasswordInpit/SuperPasswordInput";
 
 
 type FormikErrorType = {
@@ -19,9 +18,6 @@ type FormikErrorType = {
 }
 
 export const LoginForm = () => {
-
-	const [values, setValues] = useState({showPassword: false, showSnackBar: false});
-
 	const dispatch = useAppDispatch();
 	const formik = useFormik({
 		initialValues: {
@@ -31,111 +27,88 @@ export const LoginForm = () => {
 		},
 		validate: (values) => {
 			const errors: FormikErrorType = {};
-
 			if (!values.email) {
-				errors.email = 'Required';
+				errors.email = 'required';
 			} else if (!/^[A-Z\d._%+-]+@[A-Z\d.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-				errors.email = 'Invalid email address';
+				errors.email = 'invalid email address';
 			}
 			if (!values.password) {
-				errors.password = 'Required'
-			} else if (values.password.trim().length < 3) {
-				errors.password = "Min 3 symbols"
+				errors.password = 'required'
+			} else if (values.password.trim().length < 5) {
+				errors.password = "min 5 symbols"
 			}
 			return errors;
 		},
-
 		onSubmit: values => {
 			dispatch(loginThunkTC(values.email, values.password, values.rememberMe));
+			formik.resetForm();
+			formik.setTouched({});
+			formik.setErrors({email: undefined, password: undefined, rememberMe: undefined});
 		},
 	});
-
-
-	const handleClickShowPassword = () => {
-		setValues({
-			...values,
-			showPassword: !values.showPassword,
-		});
-	};
-
 	return (
 		<>
-			<form onSubmit={formik.handleSubmit}>
+			<form onSubmit={formik.handleSubmit} className={s.form}>
 				<div className={s.email}>
-
-					{/*<TextField id="standard-basic" label="Email" variant="standard"*/}
-					{/*		   type="email" {...formik.getFieldProps('email')}/>*/}
-
-					{/*заменил TextField из MaterialUI на SuperInputText:*/}
 					<SuperInputText
 						id="standard-basic"
 						type="email" {...formik.getFieldProps('email')}
 						className={s.superInputText}
-					/>
-					{formik.errors.email && formik.touched.email &&
-                        <div style={{color: "red"}}>{formik.errors.email}</div>}
-
+						error={formik.errors.email && formik.touched.email ? formik.errors.email : ''}
+						placeholder={"enter email"}/>
 				</div>
-
-				<div>
-					<FormControl sx={{m: 1, width: '25ch'}} variant="outlined">
-						<InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-						<OutlinedInput
-							id="outlined-adornment-password"
-							type={values.showPassword ? 'text' : 'password'}
-
-							{...formik.getFieldProps('password')}
-
-							endAdornment={
-								<InputAdornment position="end" >
-									<IconButton
-										aria-label="toggle password visibility"
-										onClick={handleClickShowPassword}
-										edge="end"
-									>
-										{values.showPassword ? <VisibilityOff/> : <Visibility/>}
-									</IconButton>
-								</InputAdornment>
-							}
-							label="Password"
-							autoComplete="on"
-						/>
-					</FormControl>
-
-					{formik.errors.password && formik.touched.password &&
-                        <div style={{color: "red"}}>{formik.errors.password}</div>}
-
+				<div className={s.password}>
+					<SuperPasswordInput
+						id="standard-basic"
+						type="password" {...formik.getFieldProps('password')}
+						className={s.superInputText}
+						error={formik.errors.password && formik.touched.password ? formik.errors.password : ''}
+						placeholder={"enter password"}/>
 				</div>
-				{/*<Checkbox*/}
-				{/*	{...formik.getFieldProps('rememberMe')}*/}
-				{/*>{' '}</Checkbox>Remember me (from Material UI)*/}
 				<div className={s.superCheckbox}>
 					<SuperCheckbox
-								   {...formik.getFieldProps('rememberMe')}>
-						Remember me
+								   {...formik.getFieldProps('rememberMe')}>Remember me
 					</SuperCheckbox>
 				</div>
-
-				<div className={s.forgot_password}>
-					<NavLink to={PATH.RECOVERY_PASS}>
+					<NavLink className={s.forgot_password} to={PATH.RECOVERY_PASS}>
 						Forgot Password?
 					</NavLink>
-				</div>
+
 				<SuperButton type={'submit'} className={s.submit_button}
-							 disabled={!(formik.isValid && formik.dirty)}>
-					Login
-				</SuperButton>
+							 disabled={!(formik.isValid && formik.dirty)}>Login</SuperButton>
 			</form>
-			<span
-				className={s.signUpLabel}>Don’t have an account?</span>
+			<span className={s.signUpLabel}>Don’t have an account?</span>
 			<div className={s.sign_up}>
 				<NavLink
 					to={PATH.REGISTRATION}
-
 				>Sign Up</NavLink>
 			</div>
-
 		</>
 	)
 }
+
+
+// <FormControl sx={{m: 1, width: '25ch'}} variant="outlined">
+// 	<InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+// 	<OutlinedInput
+// 		id="outlined-adornment-password"
+// 		type={values.showPassword ? 'text' : 'password'}
+//
+// 		{...formik.getFieldProps('password')}
+//
+// 		endAdornment={
+// 			<InputAdornment position="end" >
+// 				<IconButton
+// 					aria-label="toggle password visibility"
+// 					onClick={handleClickShowPassword}
+// 					edge="end"
+// 				>
+// 					{values.showPassword ? <VisibilityOff/> : <Visibility/>}
+// 				</IconButton>
+// 			</InputAdornment>
+// 		}
+// 		label="Password"
+// 		autoComplete="on"
+// 	/>
+// </FormControl>
 
