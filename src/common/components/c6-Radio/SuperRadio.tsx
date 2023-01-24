818fa0951cf7fc1e32483f8b2d7c806ef675d018
msa <serg.ks@gmail.com>
2023-01-25 03:00:51 +0400

    fix: CardLearning

--- src/common/components/c6-Radio/SuperRadio.tsx
+++ src/common/components/c6-Radio/SuperRadio.tsx
@@ -1,48 +1,73 @@
-import React, {ChangeEvent, InputHTMLAttributes, DetailedHTMLProps} from 'react'
-import css from './SuperRadio.module.css'
+import React, {
+    ChangeEvent,
+    InputHTMLAttributes,
+    DetailedHTMLProps,
+    HTMLAttributes,
+} from 'react'
+import s from './SuperRadio.module.css'
 
-type DefaultRadioPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
+type DefaultRadioPropsType = DetailedHTMLProps<
+    InputHTMLAttributes<HTMLInputElement>,
+    HTMLInputElement
+    >
+// тип пропсов обычного спана
+type DefaultSpanPropsType = DetailedHTMLProps<
+    HTMLAttributes<HTMLSpanElement>,
+    HTMLSpanElement
+    >
 
-type SuperRadioPropsType = DefaultRadioPropsType & {
+type SuperRadioPropsType = Omit<DefaultRadioPropsType, 'type'> & {
     options?: any[]
     onChangeOption?: (option: any) => void
+
+    spanProps?: DefaultSpanPropsType // пропсы для спана
 }
 
-const SuperRadio: React.FC<SuperRadioPropsType> = (
-    {
-        type, name,
-        options, value,
-        onChange, onChangeOption,
-        ...restProps
-    }
-) => {
+const SuperRadio: React.FC<SuperRadioPropsType> = ({
+                                                       id,
+                                                       name,
+                                                       className,
+                                                       options,
+                                                       value,
+                                                       onChange,
+                                                       onChangeOption,
+                                                       spanProps,
+                                                       ...restProps
+                                                   }) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
-        // onChange, onChangeOption
         onChange && onChange(e)
         onChangeOption && onChangeOption(e.currentTarget.value)
     }
-    const radioClassName = css.radio
 
-    const mappedOptions: any[] = options ? options.map((o, i) => ( // map options with key
-        <label key={name + '-' + i}>
-            <input
-                type={'radio'}
-                name={name}
-                checked={o === value}
-                value={o}
-                onChange={onChangeCallback}
-                className={radioClassName}
-                // name, checked, value, onChange
-            />
-            {o}
-        </label>
-    )) : []
+    const finalRadioClassName = s.radio + (className ? ' ' + className : '')
+    const spanClassName = s.span + (spanProps?.className ? ' ' + spanProps.className : '')
 
-    return (
-        <>
-            {mappedOptions}
-        </>
-    )
+    const mappedOptions: any[] = options
+        ? options.map((o) => (
+            <label key={name + '-' + o.id} className={s.label}>
+                <input
+                    id={id + '-input-' + o.id}
+                    className={finalRadioClassName}
+                    type={'radio'}
+                    name={name}
+                    checked={o.id == value}
+                    value={o.id}
+                    // name, checked, value делают студенты
+                    onChange={onChangeCallback}
+                    {...restProps}
+                />
+                <span
+                    id={id + '-span-' + o.id}
+                    {...spanProps}
+                    className={spanClassName}
+                >
+                      {o.value}
+                  </span>
+            </label>
+        ))
+        : []
+
+    return <div className={s.options}>{mappedOptions}</div>
 }
 
 export default SuperRadio
--- src/features/Learning/CardLearning.tsx
+++ src/features/Learning/CardLearning.tsx
@@ -6,8 +6,8 @@ import {Navigate, useParams} from "react-router-dom";
 import {DotedLoader} from "../../common/components/c8-Loaders/DotedLoader/DotedLoader";
 import SuperButton from "../../common/components/c2-Button/SuperButton";
 import {PATH} from "../../common/constants/Path";
-import {StarRating} from "../../common/components/StarRating/StarRating";
 import {getRandomCardTC, gradeCardTC} from "../cardList/cardList-reducer";
+import SuperRadio from "../../common/components/c6-Radio/SuperRadio";
 
 
 export const CardLearning = () => {
@@ -30,6 +30,13 @@ export const CardLearning = () => {
     if (!packName) {
         return <Navigate to={PATH.PACK_LIST}/>
     }
+    const arr = [
+        { id: 1, value: 'не знаю' },
+        { id: 2, value: 'что-то помню' },
+        { id: 3, value: 'вроде помню' },
+        { id: 4, value: 'хорошо помню' },
+        { id: 5, value: 'знаю' },
+    ]
 
 
     return (isFetching ?
@@ -43,15 +50,25 @@ export const CardLearning = () => {
                     {/*</div>*/}
 
                     <div className={css.personalInfo}>
-                        <h3> Question: {randomCard.question}</h3>
-                        <p>Number of answers per question:{randomCard.shots}</p>
+                        <h3> Question: </h3>
+                        <span>{randomCard.question}</span>
                         {showAnswer && <div>
                             <p>{randomCard.answer}</p>
-                            <StarRating edit={true} value={rate} size={20} onClick={(rate) => {
-                                setRate(rate)
-                                dispatch(gradeCardTC(rate))
-                            }}/>
+
+                            <SuperRadio
+                                disabled={loadingModal}
+                                id={'hw7-super-radio'}
+                                name={'hw7-radio'}
+                                options={arr}
+                                value={rate}
+                                onChangeOption={(rate) => {
+                                    setRate(rate)
+                                    dispatch(gradeCardTC(rate))
+
+                                }}
+                            />
                         </div>}
+                        <p>Number of answers per question:{randomCard.shots}</p>
                         <div style={{ position: "absolute", bottom: "50px"}}>
                             <SuperButton disabled={loadingModal || showAnswer} onClick={() => setShowAnswer(true)}>Show answer</SuperButton>
                             <SuperButton disabled={loadingModal} onClick={onNext}>Next
